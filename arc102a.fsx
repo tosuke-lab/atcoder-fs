a131d0dc1618b763fccd5077f8137fd6682fa0c1
Tosuke <tasukeprg@gmail.com>
2020-03-29 02:15:15 +0900

    arc102a.fsx

---
+++ arc102a.fsx
@@ -0,0 +1,45 @@
+open System
+
+// prelude
+let readStr () = stdin.ReadLine()
+
+let readInt () = stdin.ReadLine() |> int
+let readInts () = stdin.ReadLine().Split() |> Array.map int64
+
+let pair = function
+| [|a; b|] -> (a, b)
+| _ -> failwith "owatta"
+
+let triple = function
+| [|a; b; c|] -> (a, b, c)
+| _ -> failwith "owatta"
+
+let inc n = n + 1
+let dec n = n - 1
+
+let inline flip f a b = f b a
+let rec fix f = fun x -> (f (fix f)) x
+
+module Option =
+  let getOr defaultValue = function
+  | Some x -> x
+  | None -> defaultValue
+
+module Array =
+  let modify (arr: _ []) i f =
+    arr.[i] <- f arr.[i]
+
+module Array2D =
+  let modify (arr: _ [,]) i j f =
+    arr.[i, j] <- f arr.[i, j]
+
+// start
+let (N, K) = readInts() |> pair
+
+// mod K = n
+let count =
+  let x=N/K
+  (pown x 3)
+  + if (K%2L)=0L then pown (if (N%K)>=(K/2L) then x+1L else x) 3 else 0L
+
+printfn "%d" count
