299c6e58482c2e007728a0f5492246f312c2f4c5
Tosuke <tasukeprg@gmail.com>
2020-04-02 01:45:25 +0900

    arc004_2

---
+++ arc004_2.fsx
@@ -0,0 +1,49 @@
+open System
+
+// prelude
+let readStr () = stdin.ReadLine()
+
+let readInt () = stdin.ReadLine() |> int
+let readInts () = stdin.ReadLine().Split() |> Array.map int
+
+let pair = function
+| [|a; b|] -> (a, b)
+| _ -> failwith "owatta"
+
+let triple = function
+| [|a; b; c|] -> (a, b, c)
+| _ -> failwith "owatta"
+
+let inc n = n + 1
+let dec n = n - 1
+
+let inline flip f a b = f b a
+let rec fix f = fun x -> (f (fix f)) x
+
+module Option =
+  let getOr defaultValue = function
+  | Some x -> x
+  | None -> defaultValue
+
+module Array =
+  let modify (arr: _ []) i f =
+    arr.[i] <- f arr.[i]
+
+module Array2D =
+  let modify (arr: _ [,]) i j f =
+    arr.[i, j] <- f arr.[i, j]
+
+// start
+let N = readInt()
+let d = [| for _ in 1..N -> readInt() |]
+
+let sumD=d|>Array.sum
+// max
+sumD |> printfn "%d"
+
+// min
+let maxD = d |> Array.max
+let restD = sumD-maxD
+let minLen= if restD>=maxD then 0 else maxD-restD
+
+printfn "%d" minLen
