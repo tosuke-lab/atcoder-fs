723a311b05e6b3c659a9471074f748ecfb04d389
Tosuke <tasukeprg@gmail.com>
2020-05-12 13:38:33 +0900

    arc058-a

---
+++ arc058_a.fsx
@@ -0,0 +1,54 @@
+open System
+
+// prelude
+let readStr () = stdin.ReadLine()
+
+let read f () = readStr() |> f
+let reads f () = readStr().Split() |> Array.map f
+
+let pair = function
+| [|a; b|] -> (a, b)
+| _ -> failwith "owatta"
+
+let triple = function
+| [|a; b; c|] -> (a, b, c)
+| _ -> failwith "owatta"
+
+let readPair f g () =
+  let (a,b) = readStr().Split() |> pair
+  (f a, g b)
+
+let readTriple f g h () =
+  let (a,b,c) = readStr().Split() |> triple
+  (f a, g b, h c)
+
+let inc n = n + 1
+let dec n = n - 1
+
+let inline flip f a b = f b a
+let rec fix f = fun x -> (f (fix f)) x
+
+module Option =
+  let getOr defaultValue = function
+  | Some x -> x
+  | None -> defaultValue
+
+module Array =
+  let modify (arr: _ []) i f =
+    arr.[i] <- f arr.[i]
+
+module Array2D =
+  let modify (arr: _ [,]) i j f =
+    arr.[i, j] <- f arr.[i, j]
+
+// start
+let (N,K) = readPair int int ()
+let D = reads int () |> Array.map (fun x -> char (x + int '0'))
+
+seq { N..100000 }
+|> Seq.filter (fun n->
+  let s = (string n).ToCharArray() |> Set.ofArray
+  D |> Array.forall (not << (fun c -> s |> Set.contains c))
+)
+|> Seq.head
+|> printfn "%d"
